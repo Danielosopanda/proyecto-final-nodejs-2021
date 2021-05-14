ee24daadcf78ee7e845b8e856fed3fa3e3441620
Danielosopanda <danielmtzgro@hotmail.com>
2021-05-13 23:34:40 -0500

    Added Employee Adding Form

---
+++ UI/JS/addUser.js
@@ -0,0 +1,26 @@
+const   addBtn = document.querySelector("#btnAdd"),
+        cancelBtn = document.querySelector("#cancelAdd"),
+        form = document.querySelector("#addUserForm"),
+        container = document.querySelector("#container");
+
+var hidden = true;
+
+addBtn.addEventListener("click", () => {
+    if(hidden) {
+        
+        
+        form.style.visibility = "visible";
+        container.style.filter = "blur(4px)";
+        form.style.display = "block";
+        hidden = false;
+    } 
+});
+
+cancelBtn.addEventListener("click", () => {
+    if(!hidden) {
+        form.style.visibility = "hidden";
+        container.style.filter = "none";
+        form.style.display = "none";
+        hidden = true;
+    }
+});
